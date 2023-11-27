8b79d7c2793d8fdc03ba240c267c9abaa316a305
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2023-11-27 14:43:18 +0530

    home_work object converter

--- Home_work/user-service/src/main/java/lk/ijse/gdse/userservice/util/Converter.java
+++ Home_work/user-service/src/main/java/lk/ijse/gdse/userservice/util/Converter.java
@@ -3,11 +3,13 @@ package lk.ijse.gdse.userservice.util;
 import lk.ijse.gdse.userservice.dto.UserDTO;
 import lk.ijse.gdse.userservice.entity.User;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Converter {
 
+    @Autowired
     ModelMapper modelMapper;
 
     public User convertToEntity(UserDTO userDTO){
